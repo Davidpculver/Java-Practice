78daf379e92ae56b45fb95e731af1af9c5d6c3f8
David Culver <culver.david.p@gmail.com>
2021-07-03 23:36:14 -0500

    java practice

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -16,25 +20,82 @@ public class Main {
 //        bedroom.getLamp().turnOn();
 //        System.out.println(bedroom.getWall1().getDirection());
 
-        Car car = new Car("Base", 6);
-        System.out.println(car.startEngine());
-        System.out.println(car.accelerate());
-        System.out.println(car.brake());
-
-        Camaro camaro = new Camaro("z28", 8);
-        System.out.println(camaro.startEngine());
-        System.out.println(camaro.accelerate());
-        System.out.println(camaro.brake());
-
+//        Car car = new Car("Base", 6);
+//        System.out.println(car.startEngine());
+//        System.out.println(car.accelerate());
+//        System.out.println(car.brake());
+//
+//        Camaro camaro = new Camaro("z28", 8);
+//        System.out.println(camaro.startEngine());
+//        System.out.println(camaro.accelerate());
+//        System.out.println(camaro.brake());
+//
         Corvette corvette = new Corvette("zl1", 8);
-        System.out.println(corvette.startEngine());
-        System.out.println(corvette.accelerate());
-        System.out.println(corvette.brake());
+//        System.out.println(corvette.startEngine());
+//        System.out.println(corvette.accelerate());
+//        System.out.println(corvette.brake());
+        System.out.println(corvette.getWheels());
+//
+//        Mustang mustang = new Mustang("GT500", 8);
+//        System.out.println(mustang.startEngine());
+//        System.out.println(mustang.accelerate());
+//        System.out.println(mustang.brake());
 
-        Mustang mustang = new Mustang("GT500", 8);
-        System.out.println(mustang.startEngine());
-        System.out.println(mustang.accelerate());
-        System.out.println(mustang.brake());
+
+
+//        Hamburger hamburger = new Hamburger("Basic", "sausage", 3.56, "white");
+//        double price = hamburger.itemizeHamburger();
+//        hamburger.addHamburgerAddition1("tomato", .50);
+//        hamburger.addHamburgerAddition2("lettuce", .75);
+//        hamburger.addHamburgerAddition3("cheese", 1.10);
+//        System.out.println("Total burger price: " + hamburger.itemizeHamburger());
+//
+//        HealthyBurger healthyBurger = new HealthyBurger("Bacon", 5.67);
+//        healthyBurger.addHamburgerAddition1("Egg", 5.43);
+//        healthyBurger.addHealthAddition1("Lentils", 3.41);
+//        System.out.println("Total for your healthy hamburger is: " + healthyBurger.itemizeHamburger());
+
+
+//From assessment:
+//        Mine:
+//        public static ArrayList<User> capitalizeRecords(ArrayList<User> users){
+//            ArrayList<User> usersCapped = new ArrayList<>();
+//            for(int i = 0; i < users.size(); i++){
+//                String first = users.get(i).getFirstName();
+//                String firstCapped = first.substring(0, 1).toUpperCase(Locale.ROOT) + first.substring(1);
+//                String last = users.get(i).getLastName();
+//                String lastCapped = last.substring(0, 1).toUpperCase(Locale.ROOT) + last.substring(1);
+//                boolean adminOrNot = users.get(i).isAdmin();
+//
+//                User cappedUser = new User(firstCapped, lastCapped, adminOrNot);
+//                usersCapped.add(cappedUser);
+//
+//                System.out.println(usersCapped.get(i).getFirstName());
+//                System.out.println(usersCapped.get(i).getLastName());
+//                System.out.println(usersCapped.get(i).isAdmin());
+//
+//            }
+//            return usersCapped;
+//        }
+//
+
+
+        String[] names = {"Rick", "Stacey", "Brad", "Becca"};
+        String x;
+        try {
+            x = names[100];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception caught!");
+            x = "";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("This will always run.");
+        }
+
+
+
+
 
     }
 }
